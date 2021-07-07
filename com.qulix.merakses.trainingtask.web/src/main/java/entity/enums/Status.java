851d30dc834b26d2ca9de46a6515dd6ad8ee0385
MerakseS <lsnwot@gmail.com>
2021-07-07 14:22:34 +0300

    #6 Incorrect display of symbols in the "Status" field

--- com.qulix.merakses.trainingtask.web/src/main/java/entity/enums/Status.java
+++ com.qulix.merakses.trainingtask.web/src/main/java/entity/enums/Status.java
@@ -1,10 +1,10 @@
 package entity.enums;
 
 public enum Status {
-    NOT_STARTED("Не начата"),
-    IN_PROGRESS("В процессе"),
-    COMPLETED("Завершена"),
-    POSTPONED("Отложена");
+    NOT_STARTED("\u041d\u0435 \u043d\u0430\u0447\u0430\u0442\u0430"),
+    IN_PROGRESS("\u0412 \u043F\u0440\u043E\u0446\u0435\u0441\u0441\u0435"),
+    COMPLETED("\u0417\u0430\u0432\u0435\u0440\u0448\u0435\u043D\u0430"),
+    POSTPONED("\u041E\u0442\u043B\u043E\u0436\u0435\u043D\u0430");
 
     private final String status;
 
