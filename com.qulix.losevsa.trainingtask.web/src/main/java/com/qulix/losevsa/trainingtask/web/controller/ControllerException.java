86c15f6ca42f48390626238270f0974da920d611
MerakseS <lsnwot@gmail.com>
2021-07-23 00:46:54 +0300

    #34 Extra constructors

--- com.qulix.losevsa.trainingtask.web/src/main/java/com/qulix/losevsa/trainingtask/web/controller/ControllerException.java
+++ com.qulix.losevsa.trainingtask.web/src/main/java/com/qulix/losevsa/trainingtask/web/controller/ControllerException.java
@@ -7,31 +7,6 @@ public class ControllerException extends RuntimeException {
 
     /**
      * Instantiates a new Controller exception.
-     */
-    public ControllerException() {
-    }
-
-    /**
-     * Instantiates a new Controller exception.
-     *
-     * @param message the message
-     */
-    public ControllerException(String message) {
-        super(message);
-    }
-
-    /**
-     * Instantiates a new Controller exception.
-     *
-     * @param message the message
-     * @param cause   the cause
-     */
-    public ControllerException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    /**
-     * Instantiates a new Controller exception.
      *
      * @param cause the cause
      */
--- com.qulix.losevsa.trainingtask.web/src/main/java/com/qulix/losevsa/trainingtask/web/repository/RepositoryException.java
+++ com.qulix.losevsa.trainingtask.web/src/main/java/com/qulix/losevsa/trainingtask/web/repository/RepositoryException.java
@@ -7,31 +7,6 @@ public class RepositoryException extends RuntimeException {
 
     /**
      * Instantiates a new Repository exception.
-     */
-    public RepositoryException() {
-    }
-
-    /**
-     * Instantiates a new Repository exception.
-     *
-     * @param message the message
-     */
-    public RepositoryException(String message) {
-        super(message);
-    }
-
-    /**
-     * Instantiates a new Repository exception.
-     *
-     * @param message the message
-     * @param cause   the cause
-     */
-    public RepositoryException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    /**
-     * Instantiates a new Repository exception.
      *
      * @param cause the cause
      */
