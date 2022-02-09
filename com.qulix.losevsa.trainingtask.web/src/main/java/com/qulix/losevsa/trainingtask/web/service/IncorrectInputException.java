005f46c59de440f220de16fe0596e8e31186c4fa
MerakseS <lsnwot@gmail.com>
2022-02-09 14:32:10 +0300

    #34 Superfluous constructors

--- com.qulix.losevsa.trainingtask.web/src/main/java/com/qulix/losevsa/trainingtask/web/service/IncorrectInputException.java
+++ com.qulix.losevsa.trainingtask.web/src/main/java/com/qulix/losevsa/trainingtask/web/service/IncorrectInputException.java
@@ -7,35 +7,10 @@ public class IncorrectInputException extends RuntimeException {
 
     /**
      * Instantiates a new Service exception.
-     */
-    public IncorrectInputException() {
-    }
-
-    /**
-     * Instantiates a new Service exception.
      *
      * @param message the message
      */
     public IncorrectInputException(String message) {
         super(message);
-    }
-
-    /**
-     * Instantiates a new Service exception.
-     *
-     * @param message the message
-     * @param cause   the cause
-     */
-    public IncorrectInputException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    /**
-     * Instantiates a new Service exception.
-     *
-     * @param cause the cause
-     */
-    public IncorrectInputException(Throwable cause) {
-        super(cause);
     }
 }
